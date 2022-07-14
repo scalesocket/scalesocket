da82ee382294585a08df0452895a66945d4ebf4a
Pietu R <pietu_r@gmx.com>
2022-07-14 22:43:33 +0300

    add websocket route

--- src/main.rs
+++ src/main.rs
@@ -6,15 +6,32 @@ use {
     cli::Config,
     futures::FutureExt,
     serde_json::json,
+    tokio::try_join,
+    warp::ws::Ws,
     warp::{self, Filter, Rejection, Reply},
 };
 
-fn main() {
+#[tokio::main]
+async fn main() {
     let config = Config::parse();
 
-    let handle_routes = warp::serve(health_route()).run(config.addr).unit_error();
+    let handle_routes = warp::serve(socket_route().or(health_route()))
+        .run(config.addr)
+        .unit_error();
 
     tracing::info! { "listening" };
+
+    let _ = try_join!(handle_routes);
+}
+
+pub fn socket_route() -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
+    warp::path!(String)
+        .and(warp::ws())
+        .map(move |_, websocket: Ws| {
+            websocket.on_upgrade(move |ws| {
+                async { () }
+            })
+        })
 }
 
 pub fn health_route() -> impl Filter<Extract = impl Reply, Error = Rejection> + Clone {
