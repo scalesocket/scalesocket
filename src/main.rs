843866d0e54ae8c00887637bcb78d466b3c81c9e
Pietu R <pietu_r@gmx.com>
2022-08-14 22:06:39 +0300

    move signal module

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,7 @@ mod logging;
 mod metrics;
 mod process;
 mod routes;
+mod signal;
 mod types;
 mod utils;
 
@@ -38,24 +39,6 @@ async fn main() {
     let handle_signal = signal::handle(routes_shutdown_tx, events_shutdown_tx).unit_error();
 
     let _ = try_join!(handle_events, handle_routes, handle_signal);
-}
-
-mod signal {
-    use crate::types::{Event, EventTx, ShutdownTx};
-    use futures::FutureExt;
-    use tokio::signal::unix::{signal, SignalKind};
-
-    pub async fn handle(routes_shutdown_tx: ShutdownTx, events_shutdown_tx: EventTx) {
-        let mut interrupt = signal(SignalKind::interrupt()).expect("failed to create signal");
-        let mut terminate = signal(SignalKind::terminate()).expect("failed to create signal");
-        let signals = futures::future::select(interrupt.recv().boxed(), terminate.recv().boxed());
-
-        signals.await;
-
-        tracing::info! { "received signal, shutting down" };
-        let _ = routes_shutdown_tx.send(());
-        let _ = events_shutdown_tx.send(Event::Shutdown);
-    }
 }
 
 #[cfg(test)]
---
+++ src/signal.rs
@@ -0,0 +1,16 @@
+
+use crate::types::{Event, EventTx, ShutdownTx};
+use futures::FutureExt;
+use tokio::signal::unix::{signal, SignalKind};
+
+pub async fn handle(routes_shutdown_tx: ShutdownTx, events_shutdown_tx: EventTx) {
+    let mut interrupt = signal(SignalKind::interrupt()).expect("failed to create signal");
+    let mut terminate = signal(SignalKind::terminate()).expect("failed to create signal");
+    let signals = futures::future::select(interrupt.recv().boxed(), terminate.recv().boxed());
+
+    signals.await;
+
+    tracing::info! { "received signal, shutting down" };
+    let _ = routes_shutdown_tx.send(());
+    let _ = events_shutdown_tx.send(Event::Shutdown);
+}
